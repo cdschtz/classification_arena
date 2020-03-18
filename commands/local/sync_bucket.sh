0b8b24920f1fd1b8627cc41c6530a2891eedd0df
Chrisplaysnow <36952951+Chrisplaysnow@users.noreply.github.com>
2020-03-18 14:49:11 +0100

    update comment

--- commands/local/sync_bucket.sh
+++ commands/local/sync_bucket.sh
@@ -1,5 +1,5 @@
 # syncs the GCloud bucket with the local data
 
-# sync the wikipedia general dataset
+# sync the wikipedia general dataset, ignore macOS DS_Store files
 gsutil -m rsync -x ".*\.DS_Store$" -r $LOCAL_DATA_PATH $BUCKET_DATA_PATH
 
