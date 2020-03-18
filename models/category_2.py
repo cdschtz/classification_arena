24c26ac414309b38c2006607aa83b75c93e0ced7
Chrisplaysnow <36952951+Chrisplaysnow@users.noreply.github.com>
2020-03-18 14:25:56 +0100

    add cnn and remove comments

--- models/category_2.py
+++ models/category_2.py
@@ -1,23 +1,13 @@
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
+from torch.nn import functional as F
+
+from utils import DEVICE
 
 
 class RNN(nn.Module):
     def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length, weights):
         super(RNN, self).__init__()
-
-        """
-        Arguments
-        ---------
-        batch_size : Size of the batch which is same as the batch_size of the data returned by the TorchText BucketIterator
-        output_size : 2 = (pos, neg)
-        hidden_sie : Size of the hidden_state of the LSTM
-        vocab_size : Size of the vocabulary containing unique words
-        embedding_length : Embeddding dimension of GloVe word embeddings
-        weights : Pre-trained GloVe word_embeddings which we will use to create our word_embedding look-up table 
-
-        """
 
         self.batch_size = batch_size
         self.output_size = output_size
@@ -32,35 +22,20 @@ class RNN(nn.Module):
 
     def forward(self, input_sentences, batch_size=None):
 
-        """
-        Parameters
-        ----------
-        input_sentence: input_sentence of shape = (batch_size, num_sequences)
-        batch_size : default = None. Used only for prediction on a single sentence after training (batch_size = 1)
-
-        Returns
-        -------
-        Output of the linear layer containing logits for pos & neg class which receives its input as the final_hidden_state of RNN.
-        logits.size() = (batch_size, output_size)
-
-        """
-
         input = self.word_embeddings(input_sentences)
         input = input.permute(1, 0, 2)
         if batch_size is None:
-            h_0 = Variable(torch.zeros(4, self.batch_size, self.hidden_size))  # 4 = num_layers*num_directions
+            h_0 = torch.zeros(4, self.batch_size, self.hidden_size)  # 4 = num_layers*num_directions
             if torch.cuda.is_available():
                 h_0.cuda()
         else:
-            h_0 = Variable(torch.zeros(4, batch_size, self.hidden_size))
+            h_0 = torch.zeros(4, batch_size, self.hidden_size)
             if torch.cuda.is_available():
                 h_0.cuda()
 
         output, h_n = self.rnn(input, h_0)
-        # h_n.size() = (4, batch_size, hidden_size)
         h_n = h_n.permute(1, 0, 2)  # h_n.size() = (batch_size, 4, hidden_size)
         h_n = h_n.contiguous().view(h_n.size()[0], h_n.size()[1] * h_n.size()[2])
-        # h_n.size() = (batch_size, 4*hidden_size)
         logits = self.label(h_n)  # logits.size() = (batch_size, output_size)
 
         return logits
@@ -69,18 +44,6 @@ class RNN(nn.Module):
 class LSTMClassifier(nn.Module):
     def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length, weights):
         super(LSTMClassifier, self).__init__()
-
-        """
-        Arguments
-        ---------
-        batch_size : Size of the batch which is same as the batch_size of the data returned by the TorchText BucketIterator
-        output_size : 2 = (pos, neg)
-        hidden_sie : Size of the hidden_state of the LSTM
-        vocab_size : Size of the vocabulary containing unique words
-        embedding_length : Embeddding dimension of GloVe word embeddings
-        weights : Pre-trained GloVe word_embeddings which we will use to create our word_embedding look-up table 
-
-        """
 
         self.batch_size = batch_size
         self.output_size = output_size
@@ -96,37 +59,118 @@ class LSTMClassifier(nn.Module):
 
     def forward(self, input_sentence, batch_size=None):
 
-        """
-        Parameters
-        ----------
-        input_sentence: input_sentence of shape = (batch_size, num_sequences)
-        batch_size : default = None. Used only for prediction on a single sentence after training (batch_size = 1)
-
-        Returns
-        -------
-        Output of the linear layer containing logits for positive & negative class which receives its input as the final_hidden_state of the LSTM
-        final_output.shape = (batch_size, output_size)
-
-        """
-
-        ''' Here we will map all the indexes present in the input sequence to the corresponding word vector using our pre-trained word_embedddins.'''
         input = self.word_embeddings(
             input_sentence)  # embedded input of shape = (batch_size, num_sequences,  embedding_length)
         input = input.permute(1, 0, 2)  # input.size() = (num_sequences, batch_size, embedding_length)
         if batch_size is None:
-            # WITHOUT CUDA
-            h_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size))
-            c_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size))
-        # WITH CUDA
-        # h_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size).cuda())  # Initial hidden state of the LSTM
-        # c_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size).cuda())  # Initial cell state of the LSTM
+            h_0 = torch.zeros(1, self.batch_size, self.hidden_size).to(DEVICE)
+            c_0 = torch.zeros(1, self.batch_size, self.hidden_size).to(DEVICE)
         else:
-            h_0 = Variable(torch.zeros(1, batch_size, self.hidden_size))
-            c_0 = Variable(torch.zeros(1, batch_size, self.hidden_size))
-        # h_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
-        # c_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
+            h_0 = torch.zeros(1, batch_size, self.hidden_size).to(DEVICE)
+            c_0 = torch.zeros(1, batch_size, self.hidden_size).to(DEVICE)
+
         output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
         final_output = self.label(final_hidden_state[
                                       -1])  # final_hidden_state.size() = (1, batch_size, hidden_size) & final_output.size() = (batch_size, output_size)
 
         return final_output
+
+
+class CNN(nn.Module):
+    def __init__(self, batch_size, output_size, in_channels, out_channels, kernel_heights, stride, padding, keep_probab,
+                 vocab_size, embedding_length, weights):
+        super(CNN, self).__init__()
+
+        self.batch_size = batch_size
+        self.output_size = output_size
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_heights = kernel_heights
+        self.stride = stride
+        self.padding = padding
+        self.vocab_size = vocab_size
+        self.embedding_length = embedding_length
+
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_length)
+        self.word_embeddings.weight = nn.Parameter(weights, requires_grad=False)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, (kernel_heights[0], embedding_length), stride, padding)
+        self.conv2 = nn.Conv2d(in_channels, out_channels, (kernel_heights[1], embedding_length), stride, padding)
+        self.conv3 = nn.Conv2d(in_channels, out_channels, (kernel_heights[2], embedding_length), stride, padding)
+        self.dropout = nn.Dropout(keep_probab)
+        self.label = nn.Linear(len(kernel_heights) * out_channels, output_size)
+
+    def conv_block(self, input, conv_layer):
+        conv_out = conv_layer(input)  # conv_out.size() = (batch_size, out_channels, dim, 1)
+        activation = F.relu(conv_out.squeeze(3))  # activation.size() = (batch_size, out_channels, dim1)
+        max_out = F.max_pool1d(activation, activation.size()[2]).squeeze(
+            2)  # maxpool_out.size() = (batch_size, out_channels)
+
+        return max_out
+
+    def forward(self, input_sentences, batch_size=None):
+
+        input = self.word_embeddings(input_sentences)
+        # input.size() = (batch_size, num_seq, embedding_length)
+        input = input.unsqueeze(1)
+        # input.size() = (batch_size, 1, num_seq, embedding_length)
+        max_out1 = self.conv_block(input, self.conv1)
+        max_out2 = self.conv_block(input, self.conv2)
+        max_out3 = self.conv_block(input, self.conv3)
+
+        all_out = torch.cat((max_out1, max_out2, max_out3), 1)
+        # all_out.size() = (batch_size, num_kernels*out_channels)
+        fc_in = self.dropout(all_out)
+        # fc_in.size()) = (batch_size, num_kernels*out_channels)
+        logits = self.label(fc_in)
+
+        return logits
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length, weights):
+        super(SelfAttention, self).__init__()
+
+        self.batch_size = batch_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.embedding_length = embedding_length
+        self.weights = weights
+
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_length)
+        self.word_embeddings.weights = nn.Parameter(weights, requires_grad=False)
+        # self.dropout = 0.8
+        self.bilstm = nn.LSTM(embedding_length, hidden_size, bidirectional=True)
+        # We will use da = 350, r = 30 & penalization_coeff = 1 as per given in the self-attention original ICLR paper
+        self.W_s1 = nn.Linear(2 * hidden_size, 350)
+        self.W_s2 = nn.Linear(350, 30)
+        self.fc_layer = nn.Linear(30 * 2 * hidden_size, 2000)
+        self.label = nn.Linear(2000, output_size)
+
+    def attention_net(self, lstm_output):
+
+        attn_weight_matrix = self.W_s2(torch.tanh(self.W_s1(lstm_output)))
+        attn_weight_matrix = attn_weight_matrix.permute(0, 2, 1)
+        attn_weight_matrix = F.softmax(attn_weight_matrix, dim=2)
+
+        return attn_weight_matrix
+
+    def forward(self, input_sentences, batch_size=None):
+
+        input = self.word_embeddings(input_sentences)
+        input = input.permute(1, 0, 2)
+        if batch_size is None:
+            h_0 = torch.zeros(2, self.batch_size, self.hidden_size).to(DEVICE)
+            c_0 = torch.zeros(2, self.batch_size, self.hidden_size).to(DEVICE)
+        else:
+            h_0 = torch.zeros(2, batch_size, self.hidden_size).to(DEVICE)
+            c_0 = torch.zeros(2, batch_size, self.hidden_size).to(DEVICE)
+
+        output, (h_n, c_n) = self.bilstm(input, (h_0, c_0))
+        output = output.permute(1, 0, 2)
+        attn_weight_matrix = self.attention_net(output)
+        hidden_matrix = torch.bmm(attn_weight_matrix, output)
+        fc_out = self.fc_layer(hidden_matrix.view(-1, hidden_matrix.size()[1] * hidden_matrix.size()[2]))
+        logits = self.label(fc_out)
+
+        return logits
