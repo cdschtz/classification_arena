97462fbb8bb1e7a4a7e4c0fcda6f29b89633769d
Chrisplaysnow <36952951+Chrisplaysnow@users.noreply.github.com>
2020-03-19 07:45:18 +0100

    cmds for syncing result files

--- commands/download_from_bucket.sh
+++ commands/download_from_bucket.sh
@@ -1,5 +1,8 @@
-# downloads from the GCloud bucket to local data
+# downloads (sync) from the GCloud bucket to local data
 
 # download the wikipedia general dataset, ignore macOS DS_Store files
 gsutil -m rsync -x ".*\.DS_Store$" -r $BUCKET_DATA_PATH $WIKI_SYNTHETIC_PATH
+
+# download (sync) the result files
+gsutil -m rsync -x ".*\.DS_Store$" -r $BUCKET_RESULTS_PATH ./results/
 
--- commands/upload_to_bucket.sh
+++ commands/upload_to_bucket.sh
@@ -1,5 +1,7 @@
-# syncs the GCloud bucket with the local data
+# uploads data to the GCloud bucket from local/remote machine
 
-# sync the wikipedia general dataset, ignore macOS DS_Store files
-gsutil -m rsync -x ".*\.DS_Store$" -r $WIKI_SYNTHETIC_PATH $BUCKET_DATA_PATH
+# upload (sync) the wikipedia general dataset, ignore macOS DS_Store files
+ gsutil -m rsync -x ".*\.DS_Store$" -r $WIKI_SYNTHETIC_PATH $BUCKET_DATA_PATH
 
+# upload (sync) the result files
+gsutil -m rsync -x ".*\.DS_Store$" -r ./results/ $BUCKET_RESULTS_PATH
