8ee5679b9ce26dc716f657b4a80c348f42c7f274
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-03-21 13:13:57 +0000

    update gitignore and upload shell script

--- commands/upload_to_bucket.sh
+++ commands/upload_to_bucket.sh
@@ -1,7 +1,7 @@
 # uploads data to the GCloud bucket from local/remote machine
 
 # upload (sync) the wikipedia general dataset, ignore macOS DS_Store files
- gsutil -m rsync -x ".*\.DS_Store$" -r $WIKI_SYNTHETIC_PATH $BUCKET_DATA_PATH
+# gsutil -m rsync -x ".*\.DS_Store$" -r $WIKI_SYNTHETIC_PATH $BUCKET_DATA_PATH
 
 # upload (sync) the result files
 gsutil -m rsync -x ".*\.DS_Store$" -r ./results/ $BUCKET_RESULTS_PATH
