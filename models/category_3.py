b18c7c90cbbede8ed2db9f131fd2cfeabe68e513
Chrisplaysnow <36952951+Chrisplaysnow@users.noreply.github.com>
2020-03-23 08:07:41 +0100

    add cat 3 pipeline

---
+++ models/category_3.py
@@ -0,0 +1,6 @@
+ALL_CLASSIFICATION_MODELS = [
+    ["bert", "bert-large-uncased"],
+    ["roberta", "roberta-large"],
+    ["distilbert", "distilbert-base-uncased"],
+    ["xlnet", "xlnet-large-cased"]
+]
